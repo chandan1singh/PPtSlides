ab325ad532ae2e9408d7e036135b94fa701ea7d5
chandansingh <himanshusinghrajput7866@gmail.com>
2023-07-07 14:52:41 +0530

    New Slides

---
+++ app.js
@@ -0,0 +1,29 @@
+const slides= document.querySelectorAll(".slide");
+var counter=0;
+slides.forEach(
+    (slide,index) => {
+        slide.style.top=`${index*100}%`
+    }
+)
+
+const goPrev = () =>{
+    if(counter>0){
+        counter--
+        slideImage()
+    }
+}
+
+const goNext = () =>{
+    if(counter<3){
+        counter++
+        slideImage()
+    }
+}
+
+const slideImage =() =>{
+    slides.forEach(
+        (slide) => {
+            slide.style.transform=`translateY(-${counter*100}%)`
+        }
+    )
+}
